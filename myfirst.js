b216455c58b7fa756f123967fe95bad0b4c5a50a
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-02-26 11:09:17 +0100

    edited response

--- myfirst.js
+++ myfirst.js
@@ -3,8 +3,9 @@ const http = require('http');
 
 http.createServer((req, res)=>{
     res.writeHead(200,{'Content-type':'text/html'});
-    res.write('Hello world!');
-    res.end();
+    // res.write('Hello world!');
+    res.end('Hello world!');
+    
     console.log('Successsful!')
 }).listen(8080,()=>{
     console.log('Server running on port: 8080');
