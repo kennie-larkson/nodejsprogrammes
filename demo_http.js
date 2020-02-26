70839e95f126271eea951742e2d2174c33c8f782
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-02-26 11:38:57 +0100

    added demo_http.js

---
+++ demo_http.js
@@ -0,0 +1,10 @@
+//Include the HTTP module
+const http = require('http');
+
+//create a server object with the http createServer object
+http.createServer((req, res)=>{
+    //
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.write('Hello World!');//write a response to client
+    res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
