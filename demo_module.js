cc7c949993f135f81659d11cca480bcc05832ca1
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-02-26 11:21:35 +0100

    added myfirstmodule.js and demo_module.js

--- demo_module.js
+++ demo_module.js
@@ -1,9 +1,11 @@
 //require the built-in http node module 
 const http = require('http');
-
+const date = require('./myfirstmodule');
 
 http.createServer((req, res)=>{
     res.writeHead(200,{'Content-type':'text/html'});
+    res.write('This current date and time are '+''+ date.myDateTime());
+    // res.write('Hello world!');
     res.end('Hello world!');
     
 }).listen(8080,()=>{
