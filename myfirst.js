f8f1850a5fbe915320041986ddf9a50cf80711b0
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-02-26 11:00:51 +0100

    first commit

---
+++ myfirst.js
@@ -0,0 +1,11 @@
+//require the built-in http node module 
+const http = require('http');
+
+http.createServer((req, res)=>{
+    res.writeHead(200,{'Content-type':'text/html'});
+    res.write('Hello world!');
+    res.end();
+    console.log('Successsful!')
+}).listen(8080,()=>{
+    console.log('Server running on port: 8080');
+});
