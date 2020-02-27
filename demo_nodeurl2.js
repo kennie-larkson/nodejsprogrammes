fec93df53b678eaaa514e108ee16a55696594000
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-02-27 14:08:42 +0100

    added demo_nodeurl2.js

---
+++ demo_nodeurl2.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+
+http.createServer((req, res)=>{
+    const q = url.parse(req.url,true);
+    const filename = "."+ q.pathname;
+    fs.readFile(filename,(err,data)=>{
+        if(err){
+            res.writeHead(404, {'Content-Type':'text/html'});
+            return res.end('404 Not Found');
+        }
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write(data);
+       return res.end();
+    });
+}).listen(8080);
